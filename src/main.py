21fffd7f572231c857199b1e2d98096cdddf16c8
Sami271 <samuel.hinnen@ost.ch>
2026-01-01 11:00:28 +0100

    possibility to combine meters added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import threading
-import sys
 import logging
 import json
 import datetime
@@ -10,9 +9,9 @@ from libs.Meter.meterClass import Meter
 
 def readOutMeterThread(
     meter: Meter,
-    start_epoch_time, 
-    stop_epoch_time, 
-    results,
+    start_epoch_time: int, 
+    stop_epoch_time: int, 
+    results: dict,
     ):
     """
     Reads meter data for a given meter within a specified time range, using cache if available.
@@ -29,14 +28,15 @@ def readOutMeterThread(
     Returns:
         None. The results dictionary is updated in place with the meter data as a pandas DataFrame.
     """
-
     try:
         # try to read from cache
         data = pd.read_pickle(f"cache/{meter.name}_{start_epoch_time}_{stop_epoch_time}.secret")
-    except Exception:
+        meter.log.info(" Data read from cache.")
+    except OSError:
         # download from meter and save to cache
         data = meter.read(start_epoch_time, stop_epoch_time)
         data.to_pickle(f"cache/{meter.name}_{start_epoch_time}_{stop_epoch_time}.secret")
+        meter.log.info(" Data cached.")
 
     results[meter.name] = data
 
@@ -61,6 +61,10 @@ def getEnergyData(
         pd.DataFrame: Combined DataFrame containing energy data from all meters, with columns 
         renamed and differences calculated.
     """
+    if start_epoch_time >= stop_epoch_time:
+        raise ValueError("The start-time has to be earlier than the stop-time.")
+    if meter_list.__len__() == 0:
+        raise MemoryError("At least one meter has to be given.")
 
     results = {}
     threads = []
@@ -79,6 +83,7 @@ def getEnergyData(
         thread.daemon = True
         threads.append(thread)
         thread.start()
+        log.debug(f" Thread to read meter \"{meter.name}\" started.")
 
     # wait for threads to be done
     try:
@@ -89,17 +94,8 @@ def getEnergyData(
                 threadsAlive = threadsAlive or thread.is_alive()
                 thread.join(1)
     except KeyboardInterrupt:
-        sys.exit(1)
-
-    # rename columns
-    for meter in meter_list:
-        results[meter.name].rename(
-            columns={
-                "Energy_Import_Wh": f"{meter.name}_meterIn_Wh",
-                "Energy_Export_Wh": f"{meter.name}_meterOut_Wh",
-            },
-            inplace=True,
-        )
+        log.warning(" Meter read has been canceled")
+        return pd.DataFrame()
 
     # merge data in to one DataFrame
     meterData = pd.DataFrame()
@@ -110,8 +106,9 @@ def getEnergyData(
                                     on="Timestamp", 
                                     how="outer",
                                     )
-        except Exception:
+        except KeyError:
             meterData = results[meter.name]
+        log.debug(f" Data from meter \"{meter.name}\" merged.")
 
     # calculate diff
     meterData.set_index("Timestamp", inplace=True)
@@ -119,6 +116,24 @@ def getEnergyData(
     meterData.reset_index(inplace=True)
 
     return meterData
+
+def combineMeters(energyDF: pd.DataFrame, importMeter: str, exportMeter: str):
+
+    # production < consumtion
+    energyDF.loc[energyDF[f"{exportMeter}_Export_Wh"] < energyDF[f"{importMeter}_Import_Wh"], "temp_Export_Wh"] = 0
+    energyDF.loc[energyDF[f"{exportMeter}_Export_Wh"] < energyDF[f"{importMeter}_Import_Wh"], "temp_Import_Wh"] = energyDF[f"{importMeter}_Import_Wh"] - energyDF[f"{exportMeter}_Export_Wh"]
+
+    # production > consumtion
+    energyDF.loc[energyDF[f"{exportMeter}_Export_Wh"] > energyDF[f"{importMeter}_Import_Wh"], "temp_Export_Wh"] = energyDF[f"{exportMeter}_Export_Wh"] - energyDF[f"{importMeter}_Import_Wh"]
+    energyDF.loc[energyDF[f"{exportMeter}_Export_Wh"] > energyDF[f"{importMeter}_Import_Wh"], "temp_Import_Wh"] = 0
+
+    energyDF[f"{importMeter}_Import_Wh"] = energyDF["temp_Import_Wh"]
+    energyDF[f"{importMeter}_Export_Wh"] = energyDF["temp_Export_Wh"]
+
+    energyDF[f"{exportMeter}_Import_Wh"] = 0
+    energyDF[f"{exportMeter}_Export_Wh"] = 0
+
+    return energyDF
 
 def calculate(energyDF: pd.DataFrame, userMeter_list: List[str]):
     """
@@ -142,25 +157,25 @@ def calculate(energyDF: pd.DataFrame, userMeter_list: List[str]):
     energyDF["totalProd_Wh"] = 0  
     energyDF["totalUse_Wh"] = 0
     for meter in userMeter_list:
-        energyDF["totalProd_Wh"] += energyDF[f"{meter}_meterOut_Wh"]
-        energyDF["totalUse_Wh"] += energyDF[f"{meter}_meterIn_Wh"]
+        energyDF["totalProd_Wh"] += energyDF[f"{meter}_Export_Wh"]
+        energyDF["totalUse_Wh"] += energyDF[f"{meter}_Import_Wh"]
 
-    energyDF["eigenV_Wh"] = energyDF["totalProd_Wh"] - energyDF["ewMeter_meterOut_Wh"]
+    energyDF["eigenV_Wh"] = energyDF["totalProd_Wh"] - energyDF["ewMeter_Export_Wh"]
     for meter in userMeter_list:
         # calc energy sold and bought
-        energyDF[f"{meter}_EnSold_Wh"] = energyDF["ewMeter_meterOut_Wh"] * (
-            energyDF[f"{meter}_meterOut_Wh"] / energyDF["totalProd_Wh"])
-        energyDF[f"{meter}_EnBought_Wh"] = energyDF["ewMeter_meterIn_Wh"] * (
-            energyDF[f"{meter}_meterIn_Wh"] / energyDF["totalUse_Wh"])
+        energyDF[f"{meter}_EnSold_Wh"] = energyDF["ewMeter_Export_Wh"] * (
+            energyDF[f"{meter}_Export_Wh"] / energyDF["totalProd_Wh"])
+        energyDF[f"{meter}_EnBought_Wh"] = energyDF["ewMeter_Import_Wh"] * (
+            energyDF[f"{meter}_Import_Wh"] / energyDF["totalUse_Wh"])
 
         # calc eigenverbrauch enregy (can be combined with energy sold)
         energyDF[f"{meter}_EnSoldInt_Wh"] = energyDF["eigenV_Wh"] * (
-            energyDF[f"{meter}_meterOut_Wh"] / energyDF["totalProd_Wh"])
+            energyDF[f"{meter}_Export_Wh"] / energyDF["totalProd_Wh"])
 
     # calc eigenverbrauch energy
     for meter in userMeter_list:
         energyDF[f"{meter}_EnBoughtInt_Wh"] = (
-            energyDF[f"{meter}_meterIn_Wh"] - energyDF[f"{meter}_EnBought_Wh"])
+            energyDF[f"{meter}_Import_Wh"] - energyDF[f"{meter}_EnBought_Wh"])
         internalFremdProduktion = 0
         for meter2 in userMeter_list:
             if not (meter == meter2):
@@ -172,8 +187,8 @@ def calculate(energyDF: pd.DataFrame, userMeter_list: List[str]):
                         energyDF[f"{meter}_EnBoughtInt_Wh"] / internalFremdProduktion))
 
     # calculate meter error    
-    energyDF["EnergyIn_Wh"] = energyDF["totalProd_Wh"] + energyDF["ewMeter_meterIn_Wh"]
-    energyDF["EnergyOut_Wh"] = energyDF["totalUse_Wh"] + energyDF["ewMeter_meterOut_Wh"]
+    energyDF["EnergyIn_Wh"] = energyDF["totalProd_Wh"] + energyDF["ewMeter_Import_Wh"]
+    energyDF["EnergyOut_Wh"] = energyDF["totalUse_Wh"] + energyDF["ewMeter_Export_Wh"]
     energyDF["EnergyError_Wh"] = energyDF["EnergyIn_Wh"] - energyDF["EnergyOut_Wh"]
     
     return energyDF
@@ -304,12 +319,14 @@ def readMeters(confData: dict):
     return getEnergyData(datetime.datetime.timestamp(startTime), datetime.datetime.timestamp(stopTime), meter_list)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
+    log = logging.getLogger("Main")
 
     mainMenu = {
         "1" : "Zähler konfigurieren",
         "2" : "Zähler auslesen",
-        "3" : "Abrechnen",
+        "3" : "Zähler kombinieren",
+        "4" : "Abrechnen",
         "9" : "beenden",
     }
 
@@ -332,6 +349,13 @@ if __name__ == "__main__":
             confData = meterConfig(confData)
         elif answer == "Zähler auslesen":
             data = readMeters(confData)
+        elif answer == "Zähler kombinieren":
+            meter1 = input("Verbrauchszähler der kombiniert werden soll: ")
+            meter2 = input("Produktionszähler der kombiniert werden soll: ")
+            try:
+                data = combineMeters(data, meter1, meter2)
+            except KeyError:
+                print(f"Einer der Zählernamen \"{meter1}\" oder \"{meter2}\" ist ungültig.")
         elif answer == "Abrechnen":
             data = calculate(data, confData["meters"].keys())
             displayResults(data, confData["meters"].keys())
